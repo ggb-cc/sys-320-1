1cb2308dcc851b0b8c102eeff5f93e48e9295eed
Griffin <griffin.goochbreault@mymail.champlain.edu>
2024-09-09 12:47:11 -0400

    Work from 9/9

---
+++ week3/FunctionsAndEventLogs1.ps1
@@ -0,0 +1,64 @@
+﻿clear
+
+#Get-EventLog System -Source Microsoft-Windows-Winlogon
+
+function LogOnOffSince ($startpoint)
+{
+
+    $loginouts = Get-EventLog System -Source Microsoft-Windows-Winlogon -After (get-date).AddDays($startpoint) 
+
+    $loginoutsTable = @()
+
+    for ($i = 0 ; $i -lt $loginouts.Count ; $i ++) 
+    {
+        $event = ""
+
+        if ($loginouts[$i].Message -ilike "*on*"){$event = "Logon"}
+        if ($loginouts[$i].Message -ilike "*off*"){$event = "Logoff"}
+
+        $userSID = New-Object -TypeName System.Security.Principal.SecurityIdentifier ($loginouts[$i].ReplacementStrings[1]) # give sid
+        $userName = $userSID.Translate([System.Security.Principal.NTAccount])
+
+        $loginoutsTable += [pscustomobject]
+        @{
+            "Time : " = $loginouts[$i].TimeWritten
+            "ID : " = $loginouts[$i].Index
+            "Event : " = $event
+            "User : " = $userName
+        }
+    }
+    $loginoutsTable
+}
+
+
+
+function PowerOnOffSince ($startpoint)
+{
+
+    $loginouts = Get-EventLog System -Source Microsoft-Windows-Winlogon -After (get-date).AddDays($startpoint) 
+
+    $loginoutsTable = @()
+
+    for ($i = 0 ; $i -lt $loginouts.Count ; $i ++) 
+    {
+        $event = ""
+
+        if ($loginouts[$i].Message -ilike "*on*"){$event = "Logon"}
+        if ($loginouts[$i].Message -ilike "*off*"){$event = "Logoff"}
+
+        $userSID = New-Object -TypeName System.Security.Principal.SecurityIdentifier ($loginouts[$i].ReplacementStrings[1]) # give sid
+        $userName = $userSID.Translate([System.Security.Principal.NTAccount])
+
+        $loginoutsTable += [pscustomobject]
+        @{
+            "Time : " = $loginouts[$i].TimeWritten
+            "ID : " = $loginouts[$i].Index
+            "Event : " = $event
+            "User : " = $userName
+        }
+    }
+    $loginoutsTable
+}
+
+
+Get-Eventlog  # | Where-Object{$_.Index }
