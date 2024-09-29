78ac0cf758ae3d280d7a7daecc19e926ffcfbc8b
Griffin <griffin.goochbreault@mymail.champlain.edu>
2024-09-29 15:46:33 -0400

    Finished work on parsing apache logs

---
+++ week4/ParsingApacheLogs.ps1
@@ -0,0 +1,22 @@
+﻿function ApacheLogs1(){
+    $logsUnformatted = Get-Content C:\xampp\apache\logs\access.log;
+    $tableRecords = @();
+
+    for ($i=0; $i -lt $logsUnformatted.Length ; $i ++){
+            $words = $logsUnformatted[$i].Split(" ");
+
+            $tableRecords += [pscustomobject]@{
+                                                "IP"       = $words[0];                 `
+                                                "Time"     = $words[3].Trim('[');       `
+                                                "Method"   = $words[5].Trim('"');       `
+                                                "Page"     = $words[6];                 `
+                                                "Protocal" = $words[7];                 `
+                                                "Response" = $words[8];                 `
+                                                "Referrer" = $words[10];                `
+                                                "Client"   = $words[11..($words.Count)];
+                                                }
+
+
+            }
+    return $tableRecords | Where-Object {$_.IP -ilike "10.*"}
+}
--- week4/main.ps1
+++ week4/main.ps1
@@ -1,7 +1,14 @@
 ﻿. (Join-path $PSScriptRoot "Apache-logs.ps1")
+. (Join-Path $PSScriptRoot "ParsingApacheLogs.ps1")
 
 clear
 
 $ips = GetApacheLog "page1" "200" "mozilla" | Group-Object IP
 
 $ips | Select-Object Count, Name
+
+Write-Host " -------------- ";
+
+$a = ApacheLogs1
+
+$a | Format-Table -AutoSize -Wrap
