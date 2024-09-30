950024e68028fccd8657368f52dcd82d221f2f53
Griffin <griffin.goochbreault@mymail.champlain.edu>
2024-09-29 22:57:00 -0400

    pushing for safety, going to finish assignment before class in the morning

--- week5/ScrapingChamplainClasses.ps1
+++ week5/ScrapingChamplainClasses.ps1
@@ -2,17 +2,43 @@
 
 #http://10.0.17.30/Courses.html
 
+function gatherClasses(){
+    $page = Invoke-WebRequest -TimeoutSec 10 http://10.0.17.30/Courses.html
 
-$page = Invoke-WebRequest -TimeoutSec 10 http://10.0.17.30/Courses.html
+    $trs = $page.ParsedHtml.body.getElementsByTagName("tr")
+    $fullTable = @()
 
-$trs = $page.ParsedHtml.body.getElementsByTagName("tr")
-$fullTable = @()
+    for ($i=1; $i -lt $trs.length; $i++)
+        {
+        $tds = $trs[$i].getElementsByTagName("td")
 
-for ($i=1; $i -le $trs.length; $i++)
+        $times = $tds[5].innerText.split("-")
+
+        $fullTable += [pscustomobject]@{
+                                        "Class Code" = $tds[0].innerText; `
+                                        "Title"      = $tds[1].innerText; `
+                                        "Days"       = $tds[4].innerText; `
+                                        "Time Start" = $times[0]; `
+                                        "Time End"   = $times[1]; `
+                                        "Instructor" = $tds[6].innerText; `
+                                        "Location"   = $tds[9].innerText; `
+            }
+
+        }
+
+    return $fullTable
+}
+
+function daysTranslater($fullTable)
 {
-$tds = $trs.getElementsByTagName("td")
 
-$times = $tds[5].innerText.Value("")
+for ($i = 0 ; $i -lt $fullTable.Length; $i++)
+{
+
+    $days = @()
+    if($fullTable[$i].Days -ilike "Mon"
+
+}
 }
 
 
